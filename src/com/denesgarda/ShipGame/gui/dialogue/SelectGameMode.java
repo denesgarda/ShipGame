aad25c3e048ca97ed826a751e26c8707fcd514ce
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2022-03-31 14:09:12 -0700

    u

--- src/com/denesgarda/ShipGame/gui/dialogue/SelectGameMode.java
+++ src/com/denesgarda/ShipGame/gui/dialogue/SelectGameMode.java
@@ -4,6 +4,7 @@ import com.denesgarda.ShipGame.Game;
 import com.denesgarda.ShipGame.Main;
 import com.denesgarda.ShipGame.gui.panels.GamePanel;
 import com.denesgarda.ShipGame.util.ImageManager;
+import com.denesgarda.ShipGame.util.Popup;
 
 import javax.swing.*;
 import java.awt.*;
@@ -33,15 +34,52 @@ public class SelectGameMode extends JFrame {
         panel.add(easy);
         JButton medium = new JButton("Medium");
         medium.setAlignmentX(Component.CENTER_ALIGNMENT);
+        medium.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SelectGameMode.super.setVisible(false);
+                Game game = new Game(Main.Variables.version, 125, 2500, 12500, Game.GameMode.EASY);
+                GamePanel gamePanel = new GamePanel(Main.window, game);
+                Main.window.setPanel(gamePanel);
+            }
+        });
         panel.add(medium);
         JButton hard = new JButton("Hard");
         hard.setAlignmentX(Component.CENTER_ALIGNMENT);
+        hard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SelectGameMode.super.setVisible(false);
+                Game game = new Game(Main.Variables.version, 100, 2000, 15000, Game.GameMode.EASY);
+                GamePanel gamePanel = new GamePanel(Main.window, game);
+                Main.window.setPanel(gamePanel);
+            }
+        });
         panel.add(hard);
         JButton impossible = new JButton("Impossible");
         impossible.setAlignmentX(Component.CENTER_ALIGNMENT);
+        impossible.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SelectGameMode.super.setVisible(false);
+                Game game = new Game(Main.Variables.version, 75, 1500, 30000, Game.GameMode.EASY);
+                GamePanel gamePanel = new GamePanel(Main.window, game);
+                Main.window.setPanel(gamePanel);
+            }
+        });
         panel.add(impossible);
         JButton custom = new JButton("Custom");
         custom.setAlignmentX(Component.CENTER_ALIGNMENT);
+        custom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Popup.error("Not Yet Implemented", "Creating custom games is not yet implemented.", false);
+                /*SelectGameMode.super.setVisible(false);
+                Game game = new Game(Main.Variables.version, 150, 3000, 10000, Game.GameMode.EASY);
+                GamePanel gamePanel = new GamePanel(Main.window, game);
+                Main.window.setPanel(gamePanel);*/
+            }
+        });
         panel.add(custom);
         this.setVisible(true);
         this.setContentPane(panel);
